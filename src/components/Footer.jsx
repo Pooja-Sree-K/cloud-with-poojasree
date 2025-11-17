7036503329c47ff760ae14a821e4eb50a2a68723
Pooja <poojakarapureddy@gmail.com>
2025-11-17 10:33:20 -0600

    profilepic update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Button";
 import { socialMedia, aboutMe } from "../constants";
-import profilePic from "../assets/profile-pic.JPG";
+import profilePic from "../assets/profile-pic.jpeg";
 import { layout } from "../style";
 import { resumeLink, repoLink } from "../constants";
 import { AiFillGithub } from "react-icons/ai";
